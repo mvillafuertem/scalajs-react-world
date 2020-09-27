e3f1b81f7372466ad8edf212155ed3e8ac3725cc
Miguel Villafuerte <mivimur@hotmail.com>
2020-09-27 21:38:06 +0200

    chore(journal) : add log

--- modules/journal/src/main/scala/io/github/mvillafuertem/auth/LoginScreen.scala
+++ modules/journal/src/main/scala/io/github/mvillafuertem/auth/LoginScreen.scala
@@ -9,12 +9,15 @@ import japgolly.scalajs.react.React.Fragment
 import japgolly.scalajs.react.vdom.SvgTags.text
 import japgolly.scalajs.react.vdom.html_<^.{<, _}
 import japgolly.scalajs.react.{Callback, ReactEventFromInput, ScalaFnComponent}
+import org.scalajs.dom.console
 import typings.firebase.mod.User
 import typings.redux.mod.{ActionFromReducersMapObject, Dispatch}
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 import scala.scalajs.js
 import scala.scalajs.js.JSConverters._
+import scala.util.{Failure, Success}
 
 object LoginScreen {
 
@@ -40,7 +43,16 @@ object LoginScreen {
                 userCredential.user.asInstanceOf[User].uid,
                 userCredential.user.asInstanceOf[User].displayName.asInstanceOf[String]
               )))
-          })
+          }).transformWith {
+          case Failure(exception) =>
+            console.log("Error")
+            org.scalajs.dom.window.alert("Error")
+            org.scalajs.dom.window.alert(exception.getMessage)
+            Future(console.info(exception.getMessage))
+          case Success(value) =>
+            org.scalajs.dom.window.alert(js.JSON.stringify(value))
+            Future(value)
+        }
       }
 
     Fragment(
